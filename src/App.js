b2248c9839bf138c42a90aef7887ad323ce39dbf
PaulinaLL <paulinalll90@wp.pl>
2020-04-28 06:07:38 +0200

    func App -> class App, add Countdown props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from "react";
 import "./App.css";
 import Countdown from "./components/Countdown";
-function App() {
-  return (
-    <div className="App">
-      <main className="App-main">
-        <Countdown />
-      </main>
-    </div>
-  );
-}
+import Curtain from "./components/Curtain";
+import picture from "./components/Countdown/waterfall.jpg";
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: new Date("2020-04-29 16:10:00"),
+      newDeadline: "",
+      timeLeft: true,
+    };
+    this.timeCalculator = this.timeCalculator.bind(this);
+  }
+  timeCalculator() {
+    this.setState({
+      timeLeft: false,
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <main className="App-main">
+          {this.state.timeLeft ? (
+            <Countdown
+              picture={picture}
+              parentFunc={this.timeCalculator}
+              deadline={this.state.deadline}
+              newDeadline={this.state.newDeadline}
+            />
+          ) : (
+            <Curtain />
+          )}
+        </main>
+      </div>
+    );
+  }
+}
 export default App;
